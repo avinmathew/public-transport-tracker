f07b89d53700bab54cfc42d173fb2443f62d36fb
Avin Mathew <avinmathew@hotmail.com>
2018-05-11 20:10:45 +1000

    Import data script

---
+++ db/import_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /tmp
+mkdir SEQ_GTFS
+cd SEQ_GTFS
+wget https://gtfsrt.api.translink.com.au/GTFS/SEQ_GTFS.zip
+unzip SEQ_GTFS.zip
+mysql -u translink_gtfs -p translink_gtfs < {path-to-repo}/db/scripts}/5\ import\ data.sql
+mysql -u translink_gtfs -p translink_gtfs < {path-to-repo}/db/scripts/6\ create\ indexes.sql
