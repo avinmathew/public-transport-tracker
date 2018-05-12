70e3d1ed5dd48bc8f88ce3dc792c2431a78f9189
Avin Mathew <avinmathew@hotmail.com>
2018-05-12 20:35:21 +1000

    Wrap GTFS feed in a cache to avoid hammering when there are multiple clients

---
+++ cachedFeed.js
@@ -0,0 +1,86 @@
+const axios = require("axios");
+const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
+
+const URL = process.env.GTFS_REALTIME_URL;
+const LATLNG_DECIMAL_PLACES = 5; // Precision to send back to client
+
+const CACHE_INVALIDATE_TIME = 10000;
+
+module.exports = function () {
+  const cache = {
+    date: null,
+    vehicles: null,
+    refreshPromise: null
+  };
+  return {
+    get: () => {
+      // Check if the cache should be invalidated
+      if (cache.date && Date.now() - cache.date > CACHE_INVALIDATE_TIME) {
+        cache.date = null;
+        cache.vehicles = null;
+      }
+      // If not invalidated, return cached vehicles
+      if (cache.vehicles) {
+        return new Promise((resolve, reject) => {
+          resolve(cache.vehicles);
+        });
+      }
+      // If in the process of refreshing, return existing promise
+      if (cache.refreshPromise) {
+        return cache.refreshPromise;
+      }
+      // Otherwise refresh cache and return value
+      cache.refreshPromise = new Promise(async (resolve, reject) => {
+        try {
+          const response = await axios.get(URL, { responseType: "arraybuffer" });
+          const feed = GtfsRealtimeBindings.FeedMessage.decode(response.data);
+
+          // Find vehicles with lat/lng coords
+          let vehicles = feed.entity
+            .filter(e => e.vehicle && e.vehicle.position && e.vehicle.position.latitude && e.vehicle.position.longitude)
+            .reduce((acc, v) => {
+              acc[v.vehicle.trip.trip_id] = {
+                id: v.id,
+                tripId: v.vehicle.trip.trip_id,
+                route: v.vehicle.trip.route_id.split("-")[0],
+                latitude: v.vehicle.position.latitude && +v.vehicle.position.latitude.toFixed(LATLNG_DECIMAL_PLACES),
+                longitude: v.vehicle.position.longitude && +v.vehicle.position.longitude.toFixed(LATLNG_DECIMAL_PLACES)
+              };
+              return acc;
+            }, {});
+
+          // Add delay to vehicles
+          feed.entity
+            .filter(e => e.trip_update)
+            .forEach(t => {
+              const vehicle = vehicles[t.trip_update.trip.trip_id];
+              if (!vehicle) {
+                return;
+              }
+              let delay;
+              // Don't show a delay if waiting at first stop
+              if (t.trip_update.stop_time_update[0] && t.trip_update.stop_time_update[0].stop_sequence > 1) {
+                if (t.trip_update.stop_time_update[0].arrival) {
+                  delay = t.trip_update.stop_time_update[0].arrival.delay;
+                } else if (t.trip_update.stop_time_update[0].departure) {
+                  delay = t.trip_update.stop_time_update[0].departure.delay;
+                }
+              }
+              vehicle.delay = delay;
+            });
+
+
+          cache.vehicles = Object.values(vehicles);
+          cache.refreshPromise = null;
+          cache.date = Date.now();
+
+          return resolve(cache.vehicles);
+        } catch (err) {
+          cache.refreshPromise = null;
+          return reject(err);
+        }
+      });
+      return cache.refreshPromise;
+    },
+  }
+}();
--- index.js
+++ index.js
@@ -2,17 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const helmet = require("helmet");
-const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
-const axios = require("axios");
+const cachedFeed = require("./cachedFeed");
 const turf = {
   point: require("@turf/helpers").point,
   polygon: require("@turf/helpers").polygon,
   booleanPointInPolygon: require("@turf/boolean-point-in-polygon").default
 };
-
-const URL = process.env.GTFS_REALTIME_URL;
-const MAX_ROUTE_COUNT = 10; // Max number of routes before we ignore querying the database
-const LATLNG_DECIMAL_PLACES = 5; // Precision to send back to client
 
 const DIRECTION_LOOKUP = { 0: "in", 1: "out" };
 const ROUTE_TYPE_LOOKUP = { 0: "tram", 2: "rail", 3: "bus", 4: "ferry" };
@@ -49,90 +44,39 @@ app.get("/feed", async (req, res) => {
       [req.query.neLng, req.query.neLat],
     ]]);
 
-    const response = await axios.get(URL, { responseType: "arraybuffer" });
-    const feed = GtfsRealtimeBindings.FeedMessage.decode(response.data);
-
-    const tripUpdates = feed.entity.filter(e => e.trip_update);
-    const vehicles = feed.entity.filter(e => e.vehicle);
-
-    let entities = {};
-    // Add trip updates to trips to capture delay and vehicles that may not provide lat/lon coords
-    tripUpdates.forEach(t => {
-      // Ignore non-specified routes
-      const route = t.trip_update.trip.route_id.split("-")[0];
-      if (routes.length && !routes.includes(route)) {
-        return;
-      }
-
-      let delay;
-      // Don't show a delay if waiting at first stop
-      if (t.trip_update.stop_time_update[0] && t.trip_update.stop_time_update[0].stop_sequence > 1) {
-        if (t.trip_update.stop_time_update[0].arrival) {
-          delay = t.trip_update.stop_time_update[0].arrival.delay;
-        } else if (t.trip_update.stop_time_update[0].departure) {
-          delay = t.trip_update.stop_time_update[0].departure.delay;
-        }
-      }
-      entities[t.trip_update.trip.trip_id] = {
-        id: t.id,
-        tripId: t.trip_update.trip.trip_id,
-        route: route,
-        delay: delay
-      }
+    let vehicles = await cachedFeed.get();
+    // Keep vehicles on specified routes
+    if (routes.length) {
+      vehicles = vehicles.filter(v => routes.includes(v.route));
+    }
+    // Keep vehicles within specified map bounds
+    vehicles = vehicles.filter(v => {
+      const point = turf.point([v.longitude, v.latitude]);
+      return turf.booleanPointInPolygon(point, bounds);
     });
-    // Add vehicles to trips to capture vehicles emitting real time lat/lon coords
-    vehicles.forEach(v => {
-      // Ignore non-specified routes
-      const route = v.vehicle.trip.route_id.split("-")[0];
-      if (routes.length && !routes.includes(route)) {
-        return;
-      }
-
-      // Ignore vehicles outside specified map bounds
-      const latitude = v.vehicle.position.latitude && +v.vehicle.position.latitude.toFixed(LATLNG_DECIMAL_PLACES);
-      const longitude = v.vehicle.position.longitude && +v.vehicle.position.longitude.toFixed(LATLNG_DECIMAL_PLACES);
-      const point = turf.point([longitude, latitude]);
-      if (!turf.booleanPointInPolygon(point, bounds)) {
-        return;
-      }
-
-      entities[v.vehicle.trip.trip_id] = entities[v.vehicle.trip.trip_id] || {};
-      entities[v.vehicle.trip.trip_id] = {
-        ...entities[v.vehicle.trip.trip_id],
-        id: v.id,
-        tripId: v.vehicle.trip.trip_id,
-        route: route,
-        latitude: latitude,
-        longitude: longitude
-      }
-    });
-
-    // Only keep entities with a lat/lon
-    entities = Object.values(entities)
-      .filter(e => e.latitude && e.longitude);
 
     // Get direction and type of vehicle
     const trips = await knex
       .select("trip_id", "direction_id", "shape_id", "route_short_name", "route_type")
       .from("trips")
       .innerJoin("routes", "routes.route_id", "trips.route_id")
-      .whereIn("trip_id", entities.map(e => e.tripId));
+      .whereIn("trip_id", vehicles.map(v => v.tripId));
 
-    entities = entities.map(e => {
-      const trip = trips.find(t => t.trip_id === e.tripId) || {};
+    vehicles = vehicles.map(v => {
+      const trip = trips.find(t => t.trip_id === v.tripId) || {};
       return {
-        id: e.id,
-        route: e.route,
-        latitude: e.latitude,
-        longitude: e.longitude,
-        route: trip.route_short_name || e.route, // if there's no name, probably an unplanned trip
+        id: v.id,
+        route: v.route,
+        latitude: v.latitude,
+        longitude: v.longitude,
+        route: trip.route_short_name || v.route, // if there's no name, probably an unplanned trip
         routeType: ROUTE_TYPE_LOOKUP[trip.route_type],
         direction: DIRECTION_LOOKUP[trip.direction_id],
       }
     });
 
-    res.json(entities);
-  } catch(err) {
+    res.json(vehicles);
+  } catch (err) {
     console.error(err);
     res.status(500);
     res.json(err);
